ab5fd16a682d0715cf38b4ba6b9edeee6bcf3439
Adriana Cruz <drih1502@gmail.com>
2025-06-04 23:43:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,       // Porta padrão do dev server (pode mudar)
-    open: true        // Abre o navegador automaticamente ao iniciar
-  },
-  resolve: {
-    alias: {
-      '@': '/src'     // Alias para facilitar importações (ex: import X from '@/components/X.vue')
-    }
-  }
-});
+  plugins: [vue()]
+})
