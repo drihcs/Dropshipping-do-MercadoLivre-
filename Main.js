87930975d08aaedd9c28fa33971da243b0d9ec20
Adriana Cruz <drih1502@gmail.com>
2025-06-04 22:05:40 -0300

    Atualizando Main.js

--- Main.js
+++ Main.js
@@ -4,6 +4,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import AppLayout from './layouts/AppLayout.vue';
+
 import Home from './pages/Home.vue';
 import ApiPage from './pages/ApiPage.vue';
 import Fornecedores from './pages/Fornecedores.vue';
@@ -11,11 +13,17 @@ import GerarItens from './pages/GerarItens.vue';
 import MinhaLoja from './pages/MinhaLoja.vue';
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/api', component: ApiPage },
-  { path: '/fornecedores', component: Fornecedores },
-  { path: '/gerar-itens', component: GerarItens },
-  { path: '/minha-loja', component: MinhaLoja }
+  {
+    path: '/',
+    component: AppLayout,
+    children: [
+      { path: '', component: Home },
+      { path: 'api', component: ApiPage },
+      { path: 'fornecedores', component: Fornecedores },
+      { path: 'gerar-itens', component: GerarItens },
+      { path: 'minha-loja', component: MinhaLoja }
+    ]
+  }
 ];
 
 const router = createRouter({
